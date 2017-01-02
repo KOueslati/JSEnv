a5972290ef74a327242e461e31dde6c7e1affc2b
khaled OUESLATI <khaled.oueslati@zags.com>
2017-01-02 11:09:44 +0100

    post deploy prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -43,7 +43,10 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-			inject: true
+			inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: 'f368035a52d04ddaba0dd124c35845a7'
 		}),
 		// Eliminate duplicate packages when generating bundle
 		new webpack.optimize.DedupePlugin() ,
